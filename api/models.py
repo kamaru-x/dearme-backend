a87af7279d003ca17b62b881665a04856dca3982
kamarudheen <kamaru.office.com>
2024-11-30 18:12:46 +0530

    alter task model and added order field

--- api/models.py
+++ api/models.py
@@ -59,6 +59,7 @@ class Todo(models.Model):
 class Task(models.Model):
     date = models.DateField(auto_now_add=True)
     title = models.CharField(max_length=100)
+    order = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
